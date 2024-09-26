56c988a325a429236fdfd3bd7a48302af4831ba6
mdselimme <mdselimdev@gmail.com>
2024-09-26 15:53:02 +0600

    add_password_problem

--- password.js
+++ password.js
@@ -1,22 +1,31 @@
 
 
 function password(obj){
-     const pass = "";
-     for(let str in obj){
-        
+     if(typeof obj === "object"){
+         if(typeof obj.siteName === "string" && obj.siteName !== ""){
+             if(typeof obj.birthYear === "number" && obj.birthYear.toString().length === 4){
+                 if(typeof obj.name === "string" && obj.name !== ""){
+                     const pass = obj.siteName + "#" + (obj.name).toLowerCase() + "@" + obj.birthYear;
+                     return pass;
+                 }else{
+                     return "Write a proper name";
+                 }
+             }else{
+                 return "Write a proper Year";
+             }
+         }else{
+             return "Write a proper Sitename";
+         }
      }
-}
-
-
-const name = "selim";
-
-const birthYear = 1999;
-
-const siteName = "Google";
-
-
-const selim = {name: name, birthYear: birthYear; siteName: siteName};
-
-const result = password(selim);
-
-console.log("Your password is : ", result);
+     else{
+         return "Invalid Obj !";
+     }
+ };
+ 
+ const rahat = { name: "Selim", birthYear: 2019, siteName: "Google" } ;
+ 
+ 
+ const res2 = password(rahat);
+ 
+ 
+ console.log(res2)
